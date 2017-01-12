90209a0658e20868d13da660e075b9f27991896c
Svetlana Slyusareva <svetlana.slyusareva@me.com>
2017-01-12 21:42:26 +0100

    delat upp css filer, gjort general settings med media queries

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,6 +1,7 @@
 function toggleMenu() {
 var x = document.getElementById("toggle-menu");
   if (x.className === "nav-menu-links") {
+      // gör navigation menu visible genom att lägga till en ny klass
       x.className += " show-menu";
       document.getElementById("hamburg-menu").style.visibility = "hidden";
       document.getElementById("close-hamburg-menu").style.visibility = "visible";
